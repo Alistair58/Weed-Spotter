6962c1e2e1876c2b8d6d9d93329efb52905cf07f
Alistair <alistair@hanlonhome.co.uk>
2025-08-30 21:11:33 +0100

    - Works. Video can be streamed to ffplay but not VLC. There is about a 10 second delay before ffplay plays it

--- src/streamer.cpp
+++ src/streamer.cpp
@@ -24,9 +24,9 @@ Streamer::Streamer(int *argcPtr,char ***argvPtr){
 			"--nopreview",
 			"--width","640",
 			"--height","480",
-			"--framerate","25",
+			"--framerate","30",
 			"--codec","h264",
-			"-o","-",
+			"-o","-", //Output to stdout
 			(char *)NULL
 		);
 		throw std::runtime_error("execlp failed");
@@ -38,8 +38,11 @@ Streamer::Streamer(int *argcPtr,char ***argvPtr){
 	GstRTSPMountPoints *mounts = gst_rtsp_server_get_mount_points(server);
 
 	std::string pipeline =
-				"( fdsrc fd="+std::to_string(pipefd[0])+
-				" ! queue ! h264parse ! video/x-h264,stream-format=avc,alignment=au ! rtph264pay name=pay0 config-interval=1 pt=96 )";
+				"( fdsrc fd="+std::to_string(pipefd[0])+" name=picamsrc "+
+				" ! queue "+
+				" ! h264parse "+
+				" ! video/x-h264,stream-format=byte-stream,alignment=au "+
+				" ! rtph264pay name=pay0 config-interval=1 pt=96 )";
 	GstRTSPMediaFactory *factory = gst_rtsp_media_factory_new();
 	gst_rtsp_media_factory_set_launch(factory,pipeline.c_str());
 	gst_rtsp_media_factory_set_shared(factory,TRUE);
@@ -59,28 +62,11 @@ void Streamer::onMediaConfigure(GstRTSPMediaFactory *factory,
 				GstRTSPMedia *media,
 				gpointer user_data){
 	GstElement *element = gst_rtsp_media_get_element(media);
-	GstIterator *it = gst_bin_iterate_sources(GST_BIN(element));
-	GValue item = G_VALUE_INIT;
-	gboolean done = FALSE;
-
-	while(!done){
-		switch(gst_iterator_next(it,&item)){
-			case GST_ITERATOR_OK: {
-				GstElement *src = GST_ELEMENT(g_value_get_object(&item));
-				if(g_str_has_prefix(GST_ELEMENT_NAME(src),"fdsrc")){
-					g_object_set(src,"is-live",TRUE,NULL);
-				}
-				g_value_unset(&item);
-				break;
-			}
-			case GST_ITERATOR_DONE:
-			default:
-				done = TRUE;
-				break;
-		}
+	if(!element) return;
+	GstElement *fd = gst_bin_get_by_name_recurse_up(GST_BIN(element),"picamsrc");
+	if(fd){
+		g_object_set(fd,"is-live",TRUE,"do-timestamp",TRUE,NULL);
+		gst_object_unref(fd);
 	}
-	gst_iterator_free(it);
+	gst_object_unref(element);
 }
-
-
-
